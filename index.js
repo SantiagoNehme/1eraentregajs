286abb3719b4676445170a28fe60baab764c1b5f
SantiNehme <santiagonehme@hotmail.com>
2023-02-16 22:45:31 -0300

    arreglé problemas con los pedidos

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ const listaDeBebidasEncargadas = [];
 let opcion = prompt("Quiere comprar algo para tomar? en caso contrario escriba NO para salir");
 
 if(opcion != "no"){
+    console.log(alert("Las bebidas cuestan $300") + listaDeBebidasEncargadas);
+    
     const nombre = prompt("Ingrese nombre");
     
     const cantidad = parseInt(prompt("Ingrese el stock"));
@@ -33,30 +35,44 @@ if(opcion != "no"){
     const bebida = new Bebida(nombre, cantidad);
     
     listaDeBebidasEncargadas.push(Bebida);
-    opcion = prompt("Ingrese otra Bebida o NO para salir");
+    
 }else{
     console.log("No pidió bebidas")
 }
 
 
-console.log(alert("Las bebidas cuestan $300") + listaDeBebidasEncargadas);
+
 
 let precioBebidas = 300;
-if(comidas == "hamburgesa"){
+if(comidas == "hamburgesa" && opcion == "si"){
     let totalAPagar = precioBebidas + 1500;
     console.log(alert("El total a pagar con la bebida es: $"+totalAPagar+"."))
-}else if(comidas == "pizza"){
+}else if(comidas == "hamburgesa" && opcion == "no"){
+    console.log(alert("El total a pagar sin la bebida es: $" + 1500))
+}
+else if(comidas == "pizza" && opcion == "si"){
     let totalAPagar = precioBebidas + 1300;
     console.log(alert("El total a pagar con la bebida es: $"+totalAPagar+"."))
-}else if(comidas == "lomito"){
+}else if(comidas == "pizza" && opcion == "no"){
+    console.log(alert("El total a pagar sin la bebida es: $" + 1300))
+}
+else if(comidas == "lomito" && opcion == "si"){
     let totalAPagar = precioBebidas + 1600;
     console.log(alert("El total a pagar con la bebida es: $"+totalAPagar+"."))
-}else if(comidas == "papas fritas"){
+}else if(comidas == "lomito" && opcion == "no"){
+    console.log(alert("El total a pagar sin la bebida es: $" + 1600))
+}
+else if(comidas == "papas fritas" && opcion == "si"){
     let totalAPagar = precioBebidas + 600;
     console.log(alert("El total a pagar con la bebida es: $"+totalAPagar+"."))
-}else if(comidas == "chocotorta"){
+}else if(comidas == "papas fritas" && opcion == "no"){
+    console.log(alert("El total a pagar sin la bebida es: $" + 600))
+}
+else if(comidas == "chocotorta" && opcion == "si"){
     let totalAPagar = precioBebidas + 600;
     console.log(alert("El total a pagar con la bebida es: $"+totalAPagar+"."))
+}else if(comidas == "chocotorta" && opcion == "no"){
+    console.log(alert("El total a pagar sin la bebida es: $" + 600))
 }
 
 const metodosDePago = [{id:1,  metodo: "Efectivo"},
